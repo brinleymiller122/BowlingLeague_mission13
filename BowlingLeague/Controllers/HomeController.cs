5c03623e889c6467af297672b84b0c25cd8bed12
brinleymiller122 <miller.brinley@gmail.com>
2022-03-30 14:57:56 -0600

    Add project files.

---
+++ BowlingLeague/Controllers/HomeController.cs
@@ -0,0 +1,115 @@
+﻿using BowlingLeague.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Controllers
+{
+    public class HomeController : Controller
+    {
+      private BowlingDbContext _context { get; set; }
+
+        public HomeController(BowlingDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IActionResult Index(int teamid =-1)
+        {
+            ViewBag.TeamNames = _context.Teams.ToList();
+
+            if (teamid == -1)
+            {
+                
+                ViewBag.TeamInfo = _context.Teams.Where(x => x.TeamID == teamid).FirstOrDefault();
+                ViewBag.Bowlers = _context.Bowlers.ToList();
+            }
+            else
+            {
+                
+                ViewBag.TeamInfo = _context.Teams.Where(x => x.TeamID == teamid).FirstOrDefault();
+                ViewBag.Bowlers = _context.Bowlers.Where(x => x.TeamID == teamid).ToList();
+            }
+            
+            
+            var blah = _context.Bowlers.ToList();
+            return View(blah);
+        }
+
+        //FillOutApplication 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.TeamNames = _context.Teams.ToList();
+            ViewBag.Bowlers = _context.Bowlers.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Bowler b)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                //Bowler b = new Bowler();
+                _context.Add(b);
+                
+                _context.SaveChanges();
+                return View("Confirmation", b);
+            }
+            else
+            {
+                ViewBag.TeamNames = _context.Teams.ToList();
+                ViewBag.Bowlers = _context.Bowlers.ToList();
+                return View(b);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int bowlerid)
+        {
+            ViewBag.Bowlers = _context.Bowlers.ToList();
+            ViewBag.TeamNames = _context.Teams.ToList();
+            var bowlerinfo = _context.Bowlers.Single(x => x.BowlerID == bowlerid);
+            return View("Create", bowlerinfo);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Bowler b) 
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(b);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.TeamNames = _context.Teams.ToList();
+                return View(b);
+            }
+            
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int bowlerid)
+        {
+            var bowlerinfo = _context.Bowlers.Single(x => x.BowlerID == bowlerid);
+            return View(bowlerinfo);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Bowler b)
+        {
+            _context.Bowlers.Remove(b);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+       
+    }
+}
---
+++ BowlingLeague/Models/Bowler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Models
+{
+    public class Bowler
+    {
+        [Key]
+        [Required]
+        
+        public int BowlerID { get; set; }
+        [Required]
+        public string BowlerFirstName { get; set; }
+
+        [MaxLength(1, ErrorMessage = "Initial cannot be longer than 1 character")]
+        public string BowlerMiddleInit { get; set; }
+        [Required]
+        public string BowlerLastName { get; set; }
+        [Required]
+        public string BowlerAddress { get; set; }
+        [Required]
+        public string BowlerCity { get; set; }
+        [Required]
+        [MaxLength(2, ErrorMessage = "State cannot be longer than 2 characters")]
+        public string BowlerState { get; set; }
+        [Required]
+        [MaxLength(5, ErrorMessage = "State cannot be longer than 5 characters")]
+        public string BowlerZip { get; set; }
+        [MaxLength(13, ErrorMessage = "Phone number cannot be longer than 13 characters")]
+        public string BowlerPhoneNumber { get; set; }
+        [Required]
+        public int TeamID { get; set; }
+
+        public Team Team { get; set; }
+    }
+}
